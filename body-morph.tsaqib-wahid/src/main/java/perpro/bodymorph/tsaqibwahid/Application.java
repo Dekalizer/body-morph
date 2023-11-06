4c99b23455190f92a6ee5cb255d686c125dea646
Tsaqib Wahid <muhammadtsaqib@gmail.com>
2023-11-06 10:24:59 +0700

    Initial commit

---
+++ body-morph.tsaqib-wahid/src/main/java/perpro/bodymorph/tsaqibwahid/Application.java
@@ -0,0 +1,13 @@
+package perpro.bodymorph.tsaqibwahid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
